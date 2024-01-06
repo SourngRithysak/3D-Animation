a10c07a3d89587991ffd3e4354f482e2c326a55d
Sourng Rithysak <146438351+SourngRithysak@users.noreply.github.com>
2024-01-06 20:21:13 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".text", {
+    strings: ["Frontend Developer", "YouTuber", "Web Developer", "Backend Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 200,
+    loop:true
+});
